047461fed20cc8b2930bee39a767a1102759f9b1
Unknown <abdulmalik1878@gmail.com>
2018-05-08 21:28:48 +0700

    dialog gps on off

--- app/src/main/java/com/intaihere/malikabdul/intaihere/MainActivity.java
+++ app/src/main/java/com/intaihere/malikabdul/intaihere/MainActivity.java
@@ -1,7 +1,10 @@
 package com.intaihere.malikabdul.intaihere;
 
+import android.app.Activity;
+import android.location.Location;
 import android.os.Handler;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -14,6 +17,15 @@ import android.widget.FrameLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.places.Places;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 import com.intaihere.malikabdul.intaihere.menuGrup.GrupFragment;
 import com.intaihere.malikabdul.intaihere.menuHome.HomeFragment;
 import com.intaihere.malikabdul.intaihere.menuSetting.SettingFragment;
@@ -21,7 +33,32 @@ import com.intaihere.malikabdul.intaihere.menuStatus.StatusFragment;
 import com.intaihere.malikabdul.intaihere.utils.BottomNavigationViewHelper;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 
-public class MainActivity extends AppCompatActivity {
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     FrameLayout frameLayout;
     RelativeLayout relativeLayout;
@@ -29,15 +66,42 @@ public class MainActivity extends AppCompatActivity {
 
     boolean doubleBackToExitPressedOnce = false;
 
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+    private static final int ACCESS_FINE_LOCATION_INTENT_ID = 3;
+    private static final String BROADCAST_ACTION = "android.location.PROVIDERS_CHANGED";
+
+
+    private GoogleMap mMap;
+    private GoogleApiClient googleApiClient;
+    private Location mLastLocation;
+    private LocationRequest request;
+    private boolean mRequestingLocationUpdates;
+    View mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.mapsiki);
+        mapFragment.getMapAsync(this);
+
         relativeLayout = findViewById(R.id.rel_home);
         frameLayout = findViewById(R.id.fragment_container);
 
         setupBottomNavigationView();
+
+        initGoogleAPIClient();//Init Google API Client
+        checkPermissions();//Check Permission
+
+        mapView = mapFragment.getView();
+        View locationButton = ((View) mapView.findViewById(Integer.parseInt("1")).getParent()).findViewById(Integer.parseInt("2"));
+        RelativeLayout.LayoutParams rlp = (RelativeLayout.LayoutParams) locationButton.getLayoutParams();
+
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, 0);
+        rlp.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+        rlp.setMargins(0, 180, 180, 0);
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener botnav = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -47,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
             FragmentManager fragmentManager = getSupportFragmentManager();
             FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-            switch (item.getItemId()){
+            switch (item.getItemId()) {
                 case R.id.navigation_home:
                     transaction.replace(R.id.fragment_container, new HomeFragment()).commit();
                     relativeLayout.setVisibility(View.VISIBLE);
@@ -76,12 +140,12 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
-    private void setupBottomNavigationView(){
+    private void setupBottomNavigationView() {
         Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.navigasi);
         BottomNavigationViewHelper.setupBottomNavigationView(bottomNavigationViewEx);
 
-        BottomNavigationView BNV =  findViewById(R.id.navigasi);
+        BottomNavigationView BNV = findViewById(R.id.navigasi);
         BNV.setOnNavigationItemSelectedListener(botnav);
 
     }
@@ -101,8 +165,284 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void run() {
-                doubleBackToExitPressedOnce=false;
+                doubleBackToExitPressedOnce = false;
             }
         }, 2000);
     }
+
+    ///////////////////////////////
+    /* Initiate Google API Client  */
+    private void initGoogleAPIClient() {
+        //Without Google API Client Auto Location Dialog will not work
+        googleApiClient = new GoogleApiClient.Builder(MainActivity.this)
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+    }
+
+    /* Check Location Permission for Marshmallow Devices */
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                requestLocationPermission();
+            } else {
+                showSettingDialog();
+                setupLocationManager();
+            }
+        } else {
+            showSettingDialog();
+            setupLocationManager();
+        }
+    }
+
+    /*  Show Popup to access User Permission  */
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    ACCESS_FINE_LOCATION_INTENT_ID);
+
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    ACCESS_FINE_LOCATION_INTENT_ID);
+        }
+    }
+
+    /* Show Location Access Dialog */
+    private void showSettingDialog() {
+        request = new LocationRequest();
+        request.setSmallestDisplacement(10);
+        request.setFastestInterval(50000);
+        request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        request.setNumUpdates(3);
+        request.setInterval(30 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(request);
+        builder.setAlwaysShow(true); //this is the key ingredient to show dialog always when GPS is off
+
+        PendingResult<LocationSettingsResult> result =
+                LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates state = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        setInitialLocation();
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+//                        updateGPSStatus("GPS is Enabled in your device");
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(MainActivity.this, REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException e) {
+                            e.printStackTrace();
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+                }
+            }
+        });
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            // Check for the integer request code originally supplied to startResolutionForResult().
+            case REQUEST_CHECK_SETTINGS:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        Log.e("Settings", "Result OK");
+//                        updateGPSStatus("GPS is Enabled in your device");
+                        //startLocationUpdates();
+                        setInitialLocation();
+                        mRequestingLocationUpdates = true;
+                        break;
+                    case RESULT_CANCELED:
+                        Log.e("Settings", "Result Cancel");
+//                        updateGPSStatus("GPS is Disabled in your device");
+                        mRequestingLocationUpdates = false;
+                        break;
+
+                }
+                break;
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(gpsLocationReceiver, new IntentFilter(BROADCAST_ACTION));//Register broadcast receiver to check the status of GPS
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //Unregister receiver on destroy
+        if (gpsLocationReceiver != null)
+            unregisterReceiver(gpsLocationReceiver);
+    }
+
+    //Run on UI
+    private Runnable sendUpdatesToUI = new Runnable() {
+        public void run() {
+            showSettingDialog();
+        }
+    };
+
+    /* Broadcast receiver to check status of GPS */
+    private BroadcastReceiver gpsLocationReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            //If Action is Location
+            if (intent.getAction().matches(BROADCAST_ACTION)) {
+                LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+                //Check if GPS is turned ON or OFF
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    Log.e("About GPS", "GPS is Enabled in your device");
+//                    updateGPSStatus("GPS is Enabled in your device");
+                } else {
+                    //If GPS turned OFF show Location Dialog
+                    new Handler().postDelayed(sendUpdatesToUI, 10);
+                    // showSettingDialog();
+//                    updateGPSStatus("GPS is Disabled in your device");
+                    Log.e("About GPS", "GPS is Disabled in your device");
+                }
+
+            }
+        }
+    };
+
+
+    /* On Request permission method to check the permisison is granted or not for Marshmallow+ Devices  */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION_INTENT_ID: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    //If permission granted show location dialog if APIClient is not null
+                    if (googleApiClient == null) {
+                        initGoogleAPIClient();
+//                        showSettingDialog();
+                        setupLocationManager();
+                    }
+                } else {
+//                    updateGPSStatus("Location Permission denied.");
+                    Toast.makeText(MainActivity.this, "Location Permission denied.", Toast.LENGTH_SHORT).show();
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+        }
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        //This line will show your current location on Map with GPS dot
+        mMap.setMyLocationEnabled(true);
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    private void setupLocationManager() {
+        //buildGoogleApiClient();
+        if (googleApiClient == null) {
+
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .addApi(Places.GEO_DATA_API)
+                    .addApi(Places.PLACE_DETECTION_API)
+                    .build();
+            //mGoogleApiClient = new GoogleApiClient.Builder(this);
+        }
+        googleApiClient.connect();
+//        showSettingDialog();
+    }
+
+    private void setInitialLocation() {
+
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission
+                (MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        //getMarkers();
+
+        LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, request, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                mLastLocation = location;
+
+                Log.d(TAG, "setInitialLocation:moving camera to: lat: " + location.getLatitude() + ", lng: " + location.getLongitude());
+                try {
+                    LatLng positionUpdate = new LatLng(location.getLatitude(), location.getLongitude());
+                    CameraUpdate update = CameraUpdateFactory.newLatLngZoom(positionUpdate, 15);
+                    mMap.animateCamera(update);
+
+//                    sendLatitudeLongitude();
+                } catch (Exception ex) {
+
+                    ex.printStackTrace();
+                    Log.e("MapException", ex.getMessage());
+
+                }
+            }
+
+        });
+    }
+
 }
