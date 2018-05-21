006eece02f5fe2a8a2cfaeffe7887e0c8d2a9806
Unknown <abdulmalik1878@gmail.com>
2018-05-21 22:39:41 +0700

    java input status, pencarian belum

--- app/src/main/java/com/intaihere/malikabdul/intaihere/menuStatus/InputStatusActivity.java
+++ app/src/main/java/com/intaihere/malikabdul/intaihere/menuStatus/InputStatusActivity.java
@@ -1,15 +1,259 @@
 package com.intaihere.malikabdul.intaihere.menuStatus;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Base64;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.intaihere.malikabdul.intaihere.R;
+import com.intaihere.malikabdul.intaihere.utils.Server;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import static com.intaihere.malikabdul.intaihere.logReg.LoginActivity.my_shared_preferences;
 
 public class InputStatusActivity extends AppCompatActivity {
+
+    private Toolbar mActionToolbar;
+    private AutoCompleteTextView actInputAlamat;
+    private EditText etStatus;
+    private ImageView ivFotoStatus, ivTambahFoto;
+    private TextView tvHapus;
+    private RelativeLayout rlSimpan;
+    private Intent intent;
+    private Uri fileUri;
+    private Bitmap bitmap, decoded;
+    public final int REQUEST_CAMERA = 0;
+    public final int SELECT_FILE = 1;
+
+    int bitmap_size = 100; // image quality 1 - 100;
+    int max_resolution_image = 2000;
+
+    SharedPreferences sharedpreferences;
+    ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_status);
+
+        actInputAlamat  = findViewById(R.id.act_input_alamat);
+        etStatus        = findViewById(R.id.et_status);
+        ivFotoStatus    = findViewById(R.id.iv_foto_status);
+        tvHapus         = findViewById(R.id.tv_hapus_foto);
+        rlSimpan        = findViewById(R.id.rl_btn_simpan);
+        ivTambahFoto    = findViewById(R.id.iv_tambah_foto);
+
+        mActionToolbar = findViewById(R.id.tabs_input_status);
+        setSupportActionBar(mActionToolbar);
+        getSupportActionBar().setTitle("Buat Status");
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
+        ivTambahFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+
+        rlSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputTask();
+            }
+        });
+    }
+
+    //button back toolbar
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== android.R.id.home)
+            finish();
+        return super.onOptionsItemSelected(item);
+    }
+
+    //////////////////////////////IMAGE
+    private void selectImage() {
+        ivTambahFoto.setImageResource(0);
+        intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        fileUri = getOutputMediaFileUri();
+        intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, fileUri);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    public String getStringImage(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, bitmap_size, baos);
+        byte[] imageBytes = baos.toByteArray();
+        String encodedImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+        return encodedImage;
+    }
+
+    public Uri getOutputMediaFileUri() {
+        return Uri.fromFile(getOutputMediaFile());
+    }
+
+    private static File getOutputMediaFile() {
+
+        // External sdcard location
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "TrackingEye");
+
+        // Create the storage directory if it does not exist
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                Log.e("Monitoring", "Oops! Failed create Monitoring directory");
+                return null;
+            }
+        }
+
+        // Create a media file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "IMG_TrackingEye_" + timeStamp + ".jpg");
+
+        return mediaFile;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.e("onActivityResult", "requestCode " + requestCode + ", resultCode " + resultCode);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_CAMERA) {
+                try {
+                    Log.e("CAMERA", fileUri.getPath());
+
+                    bitmap = BitmapFactory.decodeFile(fileUri.getPath());
+                    setToImageView(getResizedBitmap(bitmap, max_resolution_image));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if (requestCode == SELECT_FILE && data != null && data.getData() != null) {
+                try {
+                    // mengambil gambar dari Gallery
+                    bitmap = MediaStore.Images.Media.getBitmap(InputStatusActivity.this.getContentResolver(), data.getData());
+                    setToImageView(getResizedBitmap(bitmap, max_resolution_image));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void setToImageView(Bitmap bmp) {
+        //compress image
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bmp.compress(Bitmap.CompressFormat.JPEG, bitmap_size, bytes);
+        decoded = BitmapFactory.decodeStream(new ByteArrayInputStream(bytes.toByteArray()));
+
+        //menampilkan gambar yang dipilih dari camera/gallery ke ImageView
+        ivFotoStatus.setImageBitmap(decoded);
+//        tv_upload.setVisibility(View.VISIBLE);
+    }
+
+    // fungsi resize image
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float) width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
+    ///////////////////////
+    private void inputTask(){
+        String url_inputTask = Server.URL_INPUT_TASK;
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Mengirim status...");
+        progressDialog.show();
+        sharedpreferences = getSharedPreferences(my_shared_preferences, MODE_PRIVATE);
+        final String idx = (sharedpreferences.getString("id", ""));
+        final String username = (sharedpreferences.getString("username", ""));
+        final String image = (sharedpreferences.getString("image", ""));
+        final String lokasi = actInputAlamat.getText().toString();
+        final String status = etStatus.getText().toString();
+
+        long date = System.currentTimeMillis();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        final String dateNow = dateFormat.format(date);
+
+        StringRequest request = new StringRequest(Request.Method.POST, url_inputTask, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                try {
+                    JSONObject object = new JSONObject(response);
+
+                    finish();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id_users", idx);
+                params.put("username", username);
+                params.put("tujuan", lokasi);
+                params.put("waktu", dateNow);
+                params.put("status", status);
+                params.put("foto_status", getStringImage(decoded));
+                params.put("image", image);
+                return params;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(request);
     }
 }
